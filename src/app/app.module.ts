9c80c63b11d9794081befd3a8a17a2bfd74f10b8
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-09-20 23:43:12 -0500

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PricesComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
